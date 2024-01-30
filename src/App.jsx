5c7a45463da3594af126e6b65e419e7c4913f098
Fausto Andrade <faustoantonioandradevera@gmail.com>
2024-01-30 12:44:20 -0500

    Se inplemento la busqueda de la ciudad por medio de input

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import WeatherCard from './components/WeatherCard';
+const APIkey = '57ba048776c7db85b4f8b6a5e60031dc';
 
 function App() {
-
   
   const [coords, setCoords]= useState();
   const [weather, setWeather] = useState();
   const [temp, setTemp] = useState();
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const [textInput, setTextInput] = useState('');
+  const [finder, setFinder] = useState();
+  const [hasError, setHasError] = useState(false);
   
   const success = position => {
     const obj = {
@@ -25,7 +28,7 @@ function App() {
   
   useEffect(() => {
     if (coords) {
-      const APIkey = '57ba048776c7db85b4f8b6a5e60031dc';
+      
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`;
 
       axios.get(url)
@@ -37,15 +40,55 @@ function App() {
         setTemp(obj);
         setWeather(res.data)})
 
-      .catch(err => console.log(err));      
+      .catch(err => console.log(err))
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      })
     }
   }, [coords]);
 
+  useEffect(() => {
+    if (textInput) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${APIkey}`;  
+    axios.get(url)
+      .then(res => {
+        const obj = {
+          celsius: (res.data.main.temp - 273.15).toFixed(2),
+          fahrenheit: ((res.data.main.temp - 273.15) * (9/5) + 32).toFixed(2)
+        }
+        setTemp(obj);
+        setHasError(false);
+        setFinder(res.data)
+      })
+      .catch(err => {
+        setHasError(true);
+        console.log(err);
+      });
+    }  
+  }, [textInput]);
+  
+
   return (
     <div className='app'>
-      <WeatherCard
-      weather={weather} 
-      temp={temp} />
+      {
+        isLoading ? 
+        <img className='loading' src="https://i.stack.imgur.com/hzk6C.gif" alt="" /> 
+        : 
+        textInput ?
+        <WeatherCard
+          weather={finder} 
+          temp={temp}
+          setTextInput={setTextInput}
+          hasError={hasError} />
+          :
+          <WeatherCard
+          weather={weather} 
+          temp={temp}
+          setTextInput={setTextInput}
+          hasError={hasError} />
+      }
     </div>
   )
 }
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './styles/weatherCard.css';
 
-const WeatherCard = ({ weather, temp }) => {
+const WeatherCard = ({weather, temp, setTextInput, hasError}) => {
 
     const [isCelsius, setIsCelsius] = useState(true);
 
@@ -9,33 +9,54 @@ const WeatherCard = ({ weather, temp }) => {
       setIsCelsius(!isCelsius);
     }
 
+    const city = useRef();
+
+    const handleForm = event => {
+      event.preventDefault();
+      setTextInput(city.current.value.toLowerCase().trim());
+    }
+
   return (
     <section className='weather'>
       <h1 className='weather_title'>Weather App</h1>
-        <h2 className='weather_city'>{weather?.name}, {weather?.sys.country}</h2>
-      <article className='weather_container1'>
-        <figure className='weather_fig'>
-            <img 
-                className='weather_img'
-                src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} 
-                alt="weather icon" />
-        </figure>
-        <div>
-            <h3 className='weather_clouds'>{weather?.weather[0].description}</h3>
-             <ul className='weather_info'>
-                <li><span>Wind speed: </span><span>{weather?.wind.speed} m/s</span></li>
-                <li><span>Clouds: </span><span>{weather?.clouds.all} %</span></li>
-                <li><span>Pressure: </span><span>{weather?.main.pressure} hPa</span></li>
-                <li><span>Humidity: </span><span>{weather?.main.humidity} %</span></li>
-             </ul>
-        </div>
-      </article>
-      <div className='weather_container2'>
-        <h3 className='weather_temp'>
-          {isCelsius ? temp?.celsius + ' °C' : temp?.fahrenheit + ' °F'}
-        </h3>
-        <button className='weather_btn' onClick={handleChange}>Change to {isCelsius? '°F' : '°C'}</button>
-      </div>
+      <form className="weather_form" onSubmit={handleForm}>
+        <input type="text" ref={city}/>
+        <button className='form_btn'>Search</button>
+      </form>
+      {
+        hasError ? 
+          <>
+            <h2>That city was not found</h2>
+            <h3>Please, try again</h3>
+          </>
+          :
+          <>
+           <h2 className='weather_city'>{weather?.name}, {weather?.sys.country}</h2>
+            <article className='weather_container1'>
+              <figure className='weather_fig'>
+                  <img 
+                      className='weather_img'
+                      src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} 
+                      alt="weather icon" />
+              </figure>
+              <div>
+                  <h3 className='weather_clouds'>{weather?.weather[0].description}</h3>
+                  <ul className='weather_info'>
+                      <li><span>Wind speed: </span><span>{weather?.wind.speed} m/s</span></li>
+                      <li><span>Clouds: </span><span>{weather?.clouds.all} %</span></li>
+                      <li><span>Pressure: </span><span>{weather?.main.pressure} hPa</span></li>
+                      <li><span>Humidity: </span><span>{weather?.main.humidity} %</span></li>
+                  </ul>
+              </div>
+            </article>
+            <div className='weather_container2'>
+              <h3 className='weather_temp'>
+                {isCelsius ? temp?.celsius + ' °C' : temp?.fahrenheit + ' °F'}
+              </h3>
+              <button className='weather_btn' onClick={handleChange}>Change to {isCelsius? '°F' : '°C'}</button>
+            </div>
+          </>
+      }
     </section>
   )
 }
