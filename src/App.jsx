b59000eb5bca032ad000c20984c761ae6917689f
Fausto Andrade <faustoantonioandradevera@gmail.com>
2024-01-29 23:02:53 -0500

    Proyecto sobre el clima

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
+
+function App() {
+
+  
+  const [coords, setCoords]= useState();
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
+  
+  
+  const success = position => {
+    // console.log(position);
+    const obj = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    }
+    setCoords(obj);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);    
+  }, []);
+  
+  useEffect(() => {
+    if (coords) {
+      const APIkey = '57ba048776c7db85b4f8b6a5e60031dc';
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`;
+
+      axios.get(url)
+      .then(res => {
+        const obj = {
+          celsius: (res.data.main.temp - 273.15).toFixed(2),
+          fahrenheit: ((res.data.main.temp - 273.15) * (9/5) + 32).toFixed(2)
+        }
+        setTemp(obj);
+        setWeather(res.data)})
+
+      .catch(err => console.log(err));      
+    }
+  }, [coords]);
+
+  return (
+    <div className='app'>
+      <WeatherCard
+      weather={weather} 
+      temp={temp} />
+    </div>
+  )
+}
+
+export default App;
